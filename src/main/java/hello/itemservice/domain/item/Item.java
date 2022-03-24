af575f781ab4a30fa6207673ba923b3b8aae5219
“kukim” <kukim.dev@gmail.com>
2022-03-24 22:37:17 +0900

    7.웹페이지만들기 : 상품 도메인 개발
    - Item 도메인 객체 생성
    - Memory 기반 ItemRepository 구현
    - ItemRepository 테스트 코드 작

---
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -0,0 +1,46 @@
+package hello.itemservice.domain.item;
+
+public class Item {
+    private Long id;
+    private String itemName;
+    private Integer price;
+    private Integer quantity;
+
+    public Item(String itemName, Integer price, Integer quantity) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public Integer getQuantity() {
+        return this.quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+}
